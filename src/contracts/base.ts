234cf4d11d8574427e58bd8675a243e261e55638
Mcx002 <muchlishchoeruddin96@gmail.com>
2022-09-08 06:09:31 +0700

    Add mapping route controller

--- src/contracts/base.ts
+++ src/contracts/base.ts
@@ -1,4 +1,6 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { EndpointData, MetadataEndpoints } from "../decorator/endpoints";
+import { logger } from "../lib/logger";
 
 export abstract class BaseController<T> {
     readonly name: string;
@@ -7,11 +9,44 @@ export abstract class BaseController<T> {
     }
     getRouter(): express.Router {
         const router = express.Router();
-        // Object.entries(this).forEach(([_, value]) => {
-        //     console.log(value);
-        // });
-        // router.use("/");
+        const options: EndpointData[] = Reflect.getMetadata(
+            MetadataEndpoints,
+            this
+        );
+        if (!options) {
+            logger.info("this controller hasn't endpoint yet");
+        }
+
+        for (const option of options) {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            router[option.method](
+                option.path,
+                (req: Request, res: Response) => {
+                    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                    // @ts-ignore
+                    const data = this[option.propertyKey](req, res);
+                    res.json({
+                        success: true,
+                        message: "OK",
+                        data,
+                    });
+                }
+            );
+        }
         return router;
     }
+    abstract init(app: T): void;
+}
+
+export abstract class BaseService<T> {
+    readonly name: string;
+    protected constructor(name: string) {
+        this.name = `Service.${name}`;
+    }
+    abstract init(app: T): void;
+}
+
+export abstract class BaseRepository<T> {
     abstract init(app: T): void;
 }
