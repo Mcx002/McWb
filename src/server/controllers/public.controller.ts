bd40d59b1bb51808a0ed8151d09c7313471170fd
Mcx002 <muchlishchoeruddin96@gmail.com>
2022-09-08 06:11:35 +0700

    Add controller for testing purpose

--- src/server/controllers/public.controller.ts
+++ src/server/controllers/public.controller.ts
@@ -1,11 +1,27 @@
-import { BaseController } from "../../contracts/controllers";
 import { App } from "../../contracts/app";
+import { BaseController } from "../../contracts/base";
+import { Get } from "../../decorator/endpoints";
+import express from "express";
+import { PublicService } from "../../contracts/services";
 
 export class PublicController extends BaseController<App> {
+    private publicService!: PublicService;
+
     constructor() {
         super("Public");
     }
-    init(_app: App) {
-        console.log("init Public");
+    init(app: App) {
+        const { service } = app;
+        this.publicService = service.Public;
+    }
+
+    @Get()
+    testData() {
+        return { data: this.publicService.get() };
+    }
+
+    @Get("/data/:xid/get")
+    testData2(req: express.Request) {
+        return { data: req.params.xid };
     }
 }
